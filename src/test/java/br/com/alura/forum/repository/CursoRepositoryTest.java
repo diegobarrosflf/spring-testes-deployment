1dde32fa2253d910dc15a6c1c1d88c0d48c5fb96
diego <diegobarrosflf@gmail.com>
2021-06-26 14:01:43 -0300

    Testando o repository com outro banco de dados

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -5,11 +5,15 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class CursoRepositoryTest {
 
     @Autowired
@@ -18,6 +22,7 @@ public class CursoRepositoryTest {
     @Test
     public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
         String nomeCurso = "HTML 5";
+        //popular o banco com as entidades para o teste
         Curso curso = cursoRepository.findByNome(nomeCurso);
         Assert.assertNotNull(curso);
         Assert.assertEquals(nomeCurso, curso.getNome());
